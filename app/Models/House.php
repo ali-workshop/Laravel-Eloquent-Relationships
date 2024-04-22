7458cb6b07360dece177891263dbe6c8b4ebbdeb
ALI ALI <alirafikali22@gmail.com>
2024-04-22 10:40:02 +0300

    add oneToOne Relation

--- app/Models/House.php
+++ app/Models/House.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class House extends Model
 {
     use HasFactory;
+
+
+
     #declare the relationship
+    public function user() :BelongsTo {
+
+        return $this->belongsTo(User::class);
+
+    }
+
 }
