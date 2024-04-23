c39e505937272295547b1bcd3cfd117e620572d6
ALI ALI <alirafikali22@gmail.com>
2024-04-23 07:47:41 +0300

    build the category class

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-   
+
+    protected $primaryKey='main_id';
+    public $incrementing=False ;
+
+
+   public function products():HasMany
+    {
+        return $this->hasMany(Products::class,'category_id','main_id');
+
+    }
 }
