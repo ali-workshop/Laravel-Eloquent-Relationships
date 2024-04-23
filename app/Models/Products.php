f4fd6165c453e01b0a1343de469ca9dbf8ecb419
ALI ALI <alirafikali22@gmail.com>
2024-04-23 07:37:21 +0300

    build the products table

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Products extends Model
 {
     use HasFactory;
+
+
+    public function category():BelongsTo
+    {
+        // parameter are : the class name ,the forien id for the current class
+        return $this->belongsTo(Category::class,'category_id');
+
+    }
+
 }
