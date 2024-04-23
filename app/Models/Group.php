4d93171a5dbe5ec9bb085877864923bacf5f8791
ALI ALI <alirafikali22@gmail.com>
2024-04-23 09:32:28 +0300

    build the group table

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
 {
     use HasFactory;
+
+    public function Group():BelongsToMany
+        {
+            return $this->BelongsToMany(Job::class);
+        
+        }
+
+
+
 }
