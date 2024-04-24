910ce5fdbb5d2cbb74f63dbcad782345077d89b0
ALI ALI <alirafikali22@gmail.com>
2024-04-24 08:51:47 +0300

    add comments

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -12,7 +12,8 @@ class Group extends Model
 
     public function jobs():BelongsToMany
         {
-            // the second parameter is the pivote table.
+            // the second parameter is the pivote table name. idont need it if i respect the convention.
+            // the convention is  alphabeta in the name G then J and singlaur in naming the pivote model so i call it GroupJob
             return $this->BelongsToMany(Job::class,'group_job')
             ->using(GroupJob::class)
             ->withPivot('active')
